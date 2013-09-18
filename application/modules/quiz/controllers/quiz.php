fb43df2a1618012ec92baa5c068b8d41aba216a2
Bagus Eko Saputro <bestbuper14@gmail.com>
2013-09-18 16:45:06 +0700

    rubah controllers 1

--- application/modules/quiz/controllers/quiz.php
+++ application/modules/quiz/controllers/quiz.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Quiz extends CI_Controller {
+class Quiz extends MX_Controller {
 
 	function __construct(){
 		parent::__construct();
